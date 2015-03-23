6ee65191bb514a3bb2c0843c2c36fa40e58cab88
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 10:00:28 +0530

    First test passed

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,6 +1,12 @@
 import unittest
 
 
+class Stock:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.price = None
+
+
 class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_should_be_None(self):
         stock = Stock("GOOG")
