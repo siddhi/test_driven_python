c44a7572cb027c570b53d3eba9bd4ae0fb19582c
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 11:34:45 +0530

    Out of order updates passing

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
@@ -5,14 +8,14 @@ class Stock:
 
     @property
     def price(self):
-        return self.price_history[-1] \
+        return self.price_history[-1][1] \
             if self.price_history else None
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        self.price_history.append(price)
+        bisect.insort_left(self.price_history, (timestamp, price))
 
     def is_increasing_trend(self):
-        return self.price_history[-3] < \
-            self.price_history[-2] < self.price_history[-1]
+        return self.price_history[-3][1] < \
+            self.price_history[-2][1] < self.price_history[-1][1]
