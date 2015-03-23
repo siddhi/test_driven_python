d6617e7b4ec5e086b1704301741bb34f4b1b0b7c
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 11:35:58 +0530

    Refactored to use named tuple

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,4 +1,7 @@
 import bisect
+import collections
+
+PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
 
 
 class Stock:
@@ -8,14 +11,14 @@ class Stock:
 
     @property
     def price(self):
-        return self.price_history[-1][1] \
+        return self.price_history[-1].price \
             if self.price_history else None
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        bisect.insort_left(self.price_history, (timestamp, price))
+        bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
 
     def is_increasing_trend(self):
-        return self.price_history[-3][1] < \
-            self.price_history[-2][1] < self.price_history[-1][1]
+        return self.price_history[-3].price < \
+            self.price_history[-2].price < self.price_history[-1].price
