bc2001ffe0bda4f524218244dad45b7e06e9a710
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 12:33:16 +0530

    Move Method to Class - initial move of code

--- stock_alerter/timeseries.py
+++ stock_alerter/timeseries.py
@@ -1,5 +1,6 @@
 import bisect
 import collections
+from datetime import timedelta
 
 Update = collections.namedtuple("Update", ["timestamp", "value"])
 
@@ -13,3 +14,18 @@ class TimeSeries:
 
     def update(self, timestamp, value):
         bisect.insort_left(self.series, Update(timestamp, value))
+
+    def get_closing_price_list(self, on_date, num_days, price_history):
+        closing_price_list = []
+        for i in range(num_days):
+            chk = on_date.date() - timedelta(i)
+            for price_event in reversed(price_history):
+                if price_event.timestamp.date() > chk:
+                    pass
+                if price_event.timestamp.date() == chk:
+                    closing_price_list.insert(0, price_event)
+                    break
+                if price_event.timestamp.date() < chk:
+                    closing_price_list.insert(0, price_event)
+                    break
+        return closing_price_list
