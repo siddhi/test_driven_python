88a7ab0ac8e8d7d05ce0c4c3ad4ee9481db3538b
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:16:11 +0530

    Using mock.patch as a class decorator

--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -4,8 +4,8 @@ from unittest import mock
 from ..action import PrintAction
 
 
+@mock.patch("builtins.print")
 class PrintActionTest(unittest.TestCase):
-    @mock.patch("builtins.print")
     def test_executing_action_prints_message(self, mock_print):
         action = PrintAction()
         action.execute("GOOG > $10")
