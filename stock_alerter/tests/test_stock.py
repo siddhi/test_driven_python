085a42d922e2edd47dc976f3b5c797a728236c65
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 10:31:18 +0530

    More trend tests

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -28,6 +28,11 @@ class StockTest(unittest.TestCase):
         self.goog.update(datetime(2014, 2, 13), price=8.4)
         self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
 
+
+class StockTrendTest(unittest.TestCase):
+    def setUp(self):
+        self.goog = Stock("GOOG")
+
     def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
         timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12),
                       datetime(2014, 2, 13)]
@@ -35,3 +40,19 @@ class StockTest(unittest.TestCase):
         for timestamp, price in zip(timestamps, prices):
             self.goog.update(timestamp, price)
         self.assertTrue(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_decreases(self):
+        timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12),
+                      datetime(2014, 2, 13)]
+        prices = [8, 12, 10]
+        for timestamp, price in zip(timestamps, prices):
+            self.goog.update(timestamp, price)
+        self.assertFalse(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_equal(self):
+        timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12),
+                      datetime(2014, 2, 13)]
+        prices = [8, 10, 10]
+        for timestamp, price in zip(timestamps, prices):
+            self.goog.update(timestamp, price)
+        self.assertFalse(self.goog.is_increasing_trend())
