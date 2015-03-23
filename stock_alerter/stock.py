eebb1d19993c37d014743fd14e8bee9c84bc74df
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 10:04:16 +0530

    Reorganised tests to a separate directory

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,16 +1,4 @@
-import unittest
-
-
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
         self.price = None
-
-
-class StockTest(unittest.TestCase):
-    def test_price_of_a_new_stock_class_should_be_None(self):
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
-
-if __name__ == "__main__":
-    unittest.main()
---
+++ stock_alerter/tests/test_stock.py
@@ -0,0 +1,9 @@
+import unittest
+
+from ..stock import Stock
+
+
+class StockTest(unittest.TestCase):
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        stock = Stock("GOOG")
+        self.assertIsNone(stock.price)
