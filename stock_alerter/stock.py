dcefe0399655da21c11ad4799c1f1969470c4e05
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 09:59:16 +0530

    Added first failing test

---
+++ stock_alerter/stock.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class StockTest(unittest.TestCase):
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        stock = Stock("GOOG")
+        self.assertIsNone(stock.price)
+
+if __name__ == "__main__":
+    unittest.main()
