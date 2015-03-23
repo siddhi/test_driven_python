1eaebe7fb80e9e5762d570d2ed2ba2571a253e80
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 11:46:56 +0530

    Smelly code for get_crossover_signal

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,5 +1,6 @@
 import bisect
 import collections
+from datetime import timedelta
 
 PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
 
@@ -22,3 +23,38 @@ class Stock:
     def is_increasing_trend(self):
         return self.price_history[-3].price < \
             self.price_history[-2].price < self.price_history[-1].price
+
+    def get_crossover_signal(self, on_date):
+        cpl = []
+        for i in range(11):
+            chk = on_date.date() - timedelta(i)
+            for price_event in reversed(self.price_history):
+                if price_event.timestamp.date() > chk:
+                    pass
+                if price_event.timestamp.date() == chk:
+                    cpl.insert(0, price_event)
+                    break
+                if price_event.timestamp.date() < chk:
+                    cpl.insert(0, price_event)
+                    break
+
+        # Return NEUTRAL signal
+        if len(cpl) < 11:
+            return 0
+
+        # BUY signal
+        if sum([update.price for update in cpl[-11:-1]])/10 \
+                > sum([update.price for update in cpl[-6:-1]])/5 \
+            and sum([update.price for update in cpl[-10:]])/10 \
+                < sum([update.price for update in cpl[-5:]])/5:
+                    return 1
+
+        # BUY signal
+        if sum([update.price for update in cpl[-11:-1]])/10 \
+                < sum([update.price for update in cpl[-6:-1]])/5 \
+            and sum([update.price for update in cpl[-10:]])/10 \
+                > sum([update.price for update in cpl[-5:]])/5:
+                    return -1
+
+        # NEUTRAL signal
+        return 0
--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -1,5 +1,6 @@
 import unittest
-from datetime import datetime
+import collections
+from datetime import datetime, timedelta
 
 from ..stock import Stock
 
@@ -55,3 +56,145 @@ class StockTrendTest(unittest.TestCase):
     def test_increasing_trend_is_false_if_price_equal(self):
         self.given_a_series_of_prices([8, 10, 10])
         self.assertFalse(self.goog.is_increasing_trend())
+
+
+class StockCrossOverSignalTest(unittest.TestCase):
+    def setUp(self):
+        self.goog = Stock("GOOG")
+
+    def _flatten(self, timestamps):
+        for timestamp in timestamps:
+            if not isinstance(timestamp, collections.Iterable):
+                yield timestamp
+            else:
+                for value in self._flatten(timestamp):
+                    yield value
+
+    def _generate_timestamp_for_date(self, date, price_list):
+        if not isinstance(price_list, collections.Iterable):
+            return date
+        else:
+            delta = 1.0/len(price_list)
+            return [date + i*timedelta(delta) for i in range(len(price_list))]
+
+    def _generate_timestamps(self, price_list):
+        return list(self._flatten([
+            self._generate_timestamp_for_date(datetime(2014, 2, 13) -
+                                              timedelta(i),
+                                              price_list[len(price_list)-i-1])
+            for i in range(len(price_list) - 1, -1, -1)
+            if price_list[len(price_list) - i - 1] is not None]))
+
+    def given_a_series_of_prices(self, price_list):
+        timestamps = self._generate_timestamps(price_list)
+        for timestamp, price in zip(timestamps,
+                                    list(self._flatten([p
+                                                        for p in price_list
+                                                        if p is not None]))):
+            self.goog.update(timestamp, price)
+
+    def test_generate_timestamp_returns_consecutive_dates(self):
+        price_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        expected = [
+            datetime(2014, 2, 3), datetime(2014, 2, 4), datetime(2014, 2, 5),
+            datetime(2014, 2, 6), datetime(2014, 2, 7), datetime(2014, 2, 8),
+            datetime(2014, 2, 9), datetime(2014, 2, 10), datetime(2014, 2, 11),
+            datetime(2014, 2, 12), datetime(2014, 2, 13)]
+        self.assertEqual(expected, self._generate_timestamps(price_list))
+
+    def test_generate_timestamp_skips_empty_dates(self):
+        price_list = [1, 2, 3, None, 5, 6, 7, 8, 9, 10, 11]
+        expected = [
+            datetime(2014, 2, 3), datetime(2014, 2, 4), datetime(2014, 2, 5),
+            datetime(2014, 2, 7), datetime(2014, 2, 8),
+            datetime(2014, 2, 9), datetime(2014, 2, 10), datetime(2014, 2, 11),
+            datetime(2014, 2, 12), datetime(2014, 2, 13)]
+        self.assertEqual(expected, self._generate_timestamps(price_list))
+
+    def test_generate_timestamp_handles_multiple_updates_per_date(self):
+        price_list = [1, 2, 3, [4, 3], 5, 6, 7, 8, 9, 10, 11]
+        expected = [
+            datetime(2014, 2, 3), datetime(2014, 2, 4), datetime(2014, 2, 5),
+            datetime(2014, 2, 6), datetime(2014, 2, 6, 12),
+            datetime(2014, 2, 7), datetime(2014, 2, 8),
+            datetime(2014, 2, 9), datetime(2014, 2, 10), datetime(2014, 2, 11),
+            datetime(2014, 2, 12), datetime(2014, 2, 13)]
+        self.assertEqual(expected, self._generate_timestamps(price_list))
+
+    def test_stock_with_no_data_returns_neutral(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([])
+        self.assertEqual(0,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_stock_with_less_data_returns_neutral(self):
+        """Even though the series has a downward crossover, we return neutral
+        because there are not enough data points"""
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 1])
+        self.assertEqual(0,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_stock_with_no_crossover_returns_neutral(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+        self.assertEqual(0,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_with_downward_crossover_returns_sell(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 4])
+        self.assertEqual(-1,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_with_upward_crossover_returns_buy(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 46])
+        self.assertEqual(1,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_should_only_look_at_closing_price(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            29, [5, 28], [5, 27], 26, 25, 24, 23, 22, 21, 20, [5, 46]])
+        self.assertEqual(1,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_should_be_neutral_if_not_enough_days_of_data(self):
+        """Even though we have 13 updates, they only cover 10 days"""
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            [5, 28], [5, 27], 26, 25, 24, 23, 22, 21, 20, [5, 46]])
+        self.assertEqual(0,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_should_pick_up_previous_closing_if_no_updates_for_a_day(self):
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            29, 28, 27, 26, 25, 24, 23, 22, 21, 20, None, None, 46])
+        self.assertEqual(1,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_should_have_11_days_worth_of_data(self):
+        """Should return signal even if there is less than 11 number of updates
+        as in the case where some days have no updates but we pick up the
+        previous closing price to fill in the value"""
+        date_to_check = datetime(2014, 2, 13)
+        self.given_a_series_of_prices([
+            27, 26, 25, 24, 23, 22, 21, 20, None, None, 46])
+        self.assertEqual(1,
+                         self.goog.get_crossover_signal(date_to_check))
+
+    def test_date_to_check_can_be_beyond_last_update_date(self):
+        """We have updates upto 13th, but we are checking signal on 15th.
+        It should just fill in the values for 14th and 15th since there are
+        no updates on these days"""
+        date_to_check = datetime(2014, 2, 15)
+        self.given_a_series_of_prices([
+            29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 46])
+        self.assertEqual(0,
+                         self.goog.get_crossover_signal(date_to_check))
