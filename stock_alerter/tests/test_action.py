b66df71e5d1c5046bfab86583ac512ffa1c483ae
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:14:44 +0530

    Using mock.patch

--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -6,12 +6,11 @@ from ..action import PrintAction
 
 class PrintActionTest(unittest.TestCase):
     def test_executing_action_prints_message(self):
-        mock_print = mock.Mock()
-        old_print = __builtins__["print"]
-        __builtins__["print"] = mock_print
+        patcher = mock.patch('builtins.print')
+        mock_print = patcher.start()
         try:
             action = PrintAction()
             action.execute("GOOG > $10")
             mock_print.assert_called_with("GOOG > $10")
         finally:
-            __builtins__["print"] = old_print
+            patcher.stop()
