5717f4ad5571ae88f238a55cf9bd365b4fe49497
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 14:52:08 +0530

    Hand written mock - extracting a Mock class

--- stock_alerter/tests/test_event.py
+++ stock_alerter/tests/test_event.py
@@ -3,25 +3,26 @@ import unittest
 from ..event import Event
 
 
+class Mock:
+    called = False
+    params = ()
+
+    def __call__(self, *args, **kwargs):
+        self.called = True
+        self.params = (args, kwargs)
+
+
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_an_event_is_raised(self):
-        called = False
-        def listener():
-            nonlocal called
-            called = True
-
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire()
-        self.assertTrue(called)
+        self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_right_parameters(self):
-        params = ()
-        def listener(*args, **kwargs):
-            nonlocal params
-            params = (args, kwargs)
-
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape="square")
-        self.assertEqual(((5, ), {"shape":"square"}), params)
+        self.assertEqual(((5, ), {"shape": "square"}), listener.params)
