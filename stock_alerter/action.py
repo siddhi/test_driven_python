0e0cac142a8e0bb3d226e06af1cf725b4b01a1d9
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:20:46 +0530

    EmailAction - patching the SMTP constructor

--- stock_alerter/action.py
+++ stock_alerter/action.py
@@ -1,3 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
 class PrintAction:
     def execute(self, content):
         print(content)
+
+
+class EmailAction:
+    """Send an email when a rule is matched"""
+    from_email = "alerts@stocks.com"
+
+    def __init__(self, to):
+        self.to_email = to
+
+    def execute(self, content):
+        message = MIMEText(content)
+        message["Subject"] = "New Stock Alert"
+        message["From"] = "alerts@stocks.com"
+        message["To"] = self.to_email
+        smtp = smtplib.SMTP("email.stocks.com")
+        try:
+            smtp.send_message(message)
+        finally:
+            smtp.quit()
--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -1,7 +1,8 @@
+import smtplib
 import unittest
 from unittest import mock
 
-from ..action import PrintAction
+from ..action import PrintAction, EmailAction
 
 
 @mock.patch("builtins.print")
@@ -10,3 +11,13 @@ class PrintActionTest(unittest.TestCase):
         action = PrintAction()
         action.execute("GOOG > $10")
         mock_print.assert_called_with("GOOG > $10")
+
+
+@mock.patch("smtplib.SMTP")
+class EmailActionTest(unittest.TestCase):
+    def setUp(self):
+        self.action = EmailAction(to="siddharta@silverstripesoftware.com")
+
+    def test_email_is_sent_to_the_right_server(self, mock_smtp_class):
+        self.action.execute("MSFT has crossed $10 price level")
+        mock_smtp_class.assert_called_with("email.stocks.com")
