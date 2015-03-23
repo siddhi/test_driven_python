b48fe76bcf6eb90ac0a03fc8b9d3989cee6bd706
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 14:54:40 +0530

    Checking method call parameters

--- stock_alerter/tests/test_event.py
+++ stock_alerter/tests/test_event.py
@@ -4,15 +4,6 @@ from unittest import mock
 from ..event import Event
 
 
-class Mock:
-    called = False
-    params = ()
-
-    def __call__(self, *args, **kwargs):
-        self.called = True
-        self.params = (args, kwargs)
-
-
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_an_event_is_raised(self):
         listener = mock.Mock()
@@ -22,8 +13,8 @@ class EventTest(unittest.TestCase):
         self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_right_parameters(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape="square")
-        self.assertEqual(((5, ), {"shape": "square"}), listener.params)
+        listener.assert_called_with(5, shape="square")
