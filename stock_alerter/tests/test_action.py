50480b43589442ab91e5f02eaaf8b075a8706309
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:15:44 +0530

    Using mock.patch as a decorator

--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -5,8 +5,8 @@ from ..action import PrintAction
 
 
 class PrintActionTest(unittest.TestCase):
-    def test_executing_action_prints_message(self):
-        with mock.patch('builtins.print') as mock_print:
-            action = PrintAction()
-            action.execute("GOOG > $10")
-            mock_print.assert_called_with("GOOG > $10")
+    @mock.patch("builtins.print")
+    def test_executing_action_prints_message(self, mock_print):
+        action = PrintAction()
+        action.execute("GOOG > $10")
+        mock_print.assert_called_with("GOOG > $10")
