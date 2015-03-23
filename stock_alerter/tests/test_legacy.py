c0df4ca986e56ff45e79809f4a3bd62670ae1d7e
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 18:20:31 +0530

    Second characterisation test

--- stock_alerter/tests/test_legacy.py
+++ stock_alerter/tests/test_legacy.py
@@ -12,3 +12,12 @@ class AlertProcessorTest(unittest.TestCase):
                                      mock.call("GOOG", 15),
                                      mock.call("AAPL", 10),
                                      mock.call("GOOG", 21)])
+
+    def test_processor_characterization_2(self):
+        processor = AlertProcessor(autorun=False)
+        with mock.patch("builtins.print") as mock_print:
+            processor.run()
+        mock_print.assert_has_calls([mock.call("AAPL", 8),
+                                     mock.call("GOOG", 15),
+                                     mock.call("AAPL", 10),
+                                     mock.call("GOOG", 21)])
