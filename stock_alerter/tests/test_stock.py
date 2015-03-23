0e6a5774e437e9cdbda724b0d94811db0ea98b59
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 10:17:30 +0530

    Second failing test

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from ..stock import Stock
 
@@ -7,3 +8,12 @@ class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_should_be_None(self):
         stock = Stock("GOOG")
         self.assertIsNone(stock.price)
+
+    def test_stock_update(self):
+        """An update should set the price on the stock object
+
+        We will be  using the `datetime` module for the timestamp
+        """
+        goog = Stock("GOOG")
+        goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, goog.price)
