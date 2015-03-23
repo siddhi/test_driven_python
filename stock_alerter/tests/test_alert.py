94ab2a045efdec135c1dca84d8149318d53739ee
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:11:24 +0530

    Setting mock side effects

--- stock_alerter/tests/test_alert.py
+++ stock_alerter/tests/test_alert.py
@@ -5,11 +5,15 @@ from datetime import datetime
 from ..alert import Alert
 from ..rule import PriceRule
 from ..stock import Stock
+from ..event import Event
 
 
 class AlertTest(unittest.TestCase):
     def test_action_is_executed_when_rule_matches(self):
-        exchange = {"GOOG": Stock("GOOG")}
+        goog = mock.MagicMock(spec=Stock)
+        goog.updated = Event()
+        goog.update.side_effect = lambda date, value: goog.updated.fire(self)
+        exchange = {"GOOG": goog}
         rule = mock.MagicMock(spec=PriceRule)
         rule.matches.return_value = True
         rule.depends_on.return_value = {"GOOG"}
