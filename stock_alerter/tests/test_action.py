adc96064fa242c69aecd298802e20ff5a0b96eb0
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:23:07 +0530

    EmailAction - verifying the calls to SMTP

--- stock_alerter/tests/test_action.py
+++ stock_alerter/tests/test_action.py
@@ -21,3 +21,12 @@ class EmailActionTest(unittest.TestCase):
     def test_email_is_sent_to_the_right_server(self, mock_smtp_class):
         self.action.execute("MSFT has crossed $10 price level")
         mock_smtp_class.assert_called_with("email.stocks.com")
+
+    def test_connection_closed_after_sending_mail(self, mock_smtp_class):
+        mock_smtp = mock_smtp_class.return_value
+        self.action.execute("MSFT has crossed $10 price level")
+        mock_smtp.send_message.assert_called_with(mock.ANY)
+        self.assertTrue(mock_smtp.quit.called)
+        mock_smtp.assert_has_calls([
+            mock.call.send_message(mock.ANY),
+            mock.call.quit()])
