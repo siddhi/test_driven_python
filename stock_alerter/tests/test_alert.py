fce64478f72583b0acc258f8132778348be35d14
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-23 15:08:39 +0530

    Testing interactions approach 2 - mocking objects

--- stock_alerter/tests/test_alert.py
+++ stock_alerter/tests/test_alert.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 from datetime import datetime
 
 from ..alert import Alert
@@ -6,19 +7,12 @@ from ..rule import PriceRule
 from ..stock import Stock
 
 
-class TestAction:
-    executed = False
-
-    def execute(self, description):
-        self.executed = True
-
-
 class AlertTest(unittest.TestCase):
     def test_action_is_executed_when_rule_matches(self):
         exchange = {"GOOG": Stock("GOOG")}
         rule = PriceRule("GOOG", lambda stock: stock.price > 10)
-        action = TestAction()
+        action = mock.MagicMock()
         alert = Alert("sample alert", rule, action)
         alert.connect(exchange)
         exchange["GOOG"].update(datetime(2014, 2, 10), 11)
-        self.assertTrue(action.executed)
+        action.execute.assert_called_with("sample alert")
