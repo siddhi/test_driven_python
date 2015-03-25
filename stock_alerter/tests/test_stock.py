ac686a1a23e334f3a092cf20be26be7d124cc530
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-25 09:22:25 +0530

    nose2 - parametrised tests

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -2,6 +2,8 @@ import unittest
 import collections
 from datetime import datetime, timedelta
 
+from nose2.tools.params import params
+
 from ..stock import Stock, StockSignal
 
 
@@ -19,6 +21,24 @@ def test_price_of_a_new_stock_class_should_be_None():
     assert goog.price is None
 test_price_of_a_new_stock_class_should_be_None.setup = setup_test
 test_price_of_a_new_stock_class_should_be_None.teardown = teardown_test
+
+
+def given_a_series_of_prices(stock, prices):
+    timestamps = [datetime(2014, 2, 10), datetime(2014, 2, 11),
+                  datetime(2014, 2, 12), datetime(2014, 2, 13)]
+    for timestamp, price in zip(timestamps, prices):
+        stock.update(timestamp, price)
+
+
+@params(
+    ([8, 10, 12], True),
+    ([8, 12, 10], False),
+    ([8, 10, 10], False)
+)
+def test_stock_trends(prices, expected_output):
+    goog = Stock("GOOG")
+    given_a_series_of_prices(goog, prices)
+    assert goog.is_increasing_trend() == expected_output
 
 
 class StockTest(unittest.TestCase):
