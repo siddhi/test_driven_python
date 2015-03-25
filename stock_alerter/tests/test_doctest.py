d57f1c30d6992394ef608740b343b0d6cb65deb5
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-25 08:53:37 +0530

    Using load_tests protocol to create a unittest compatible test suite

--- stock_alerter/tests/test_doctest.py
+++ stock_alerter/tests/test_doctest.py
@@ -1,11 +1,8 @@
 import doctest
-import unittest
 from stock_alerter import stock
 
 
-class PackageDocTest(unittest.TestCase):
-    def test_stock_module(self):
-        doctest.testmod(stock)
-
-    def test_doc(self):
-        doctest.testfile(r"..\readme.txt")
+def load_tests(loader, tests, pattern):
+    tests.addTests(doctest.DocTestSuite(stock))
+    tests.addTests(doctest.DocFileSuite("../readme.txt"))
+    return tests
