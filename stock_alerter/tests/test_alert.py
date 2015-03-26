21f8b9977798409ba1d3ade1d97999ed28c08f47
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-03-26 08:49:38 +0530

    Implementing a spy

--- stock_alerter/tests/test_alert.py
+++ stock_alerter/tests/test_alert.py
@@ -22,3 +22,15 @@ class AlertTest(unittest.TestCase):
         alert.connect(exchange)
         exchange["GOOG"].update(datetime(2014, 2, 10), 11)
         action.execute.assert_called_with("sample alert")
+
+    def test_action_doesnt_fire_if_rule_doesnt_match(self):
+        goog = Stock("GOOG")
+        exchange = {"GOOG": goog}
+        rule = PriceRule("GOOG", lambda stock: stock.price > 10)
+        rule_spy = mock.MagicMock(wraps=rule)
+        action = mock.MagicMock()
+        alert = Alert("sample alert", rule_spy, action)
+        alert.connect(exchange)
+        alert.check_rule(goog)
+        rule_spy.matches.assert_called_with(exchange)
+        self.assertFalse(action.execute.called)
